383f72398b4b7b72da036f3d8d14faa2c733a7ba
Lee Robinson <lrobinson2011@gmail.com>
2022-03-02 20:39:02 -0600

    Update dependencies

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# https://reactions-demo.vercel.app
+# Incremental Static Regeneration (Demo)
+
+**Demo: https://reactions-demo.vercel.app**
+
+## About
+
+This demo showcases [Incremental Static Regeneration](https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration) inside Next.js, using GitHub issues as a data source.
+
+With Next.js 12.1, we can now revalidate on-demand. Check out the [latest demo here](https://on-demand-isr.vercel.app/) for more information.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export async function getStaticProps() {
   // {
@@ -22,11 +22,11 @@ export async function getStaticProps() {
   const res = await fetch('https://api.github.com/graphql', {
     method: 'POST',
     headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
     },
     body: JSON.stringify({
       query: `query {
-        repository(owner:"chibicode", name:"reactions") {
+        repository(owner:"vercel", name:"reactions") {
           issue(number:1) {
             reactionGroups {
               content
@@ -36,44 +36,44 @@ export async function getStaticProps() {
             }
           }
         }
-      }`
-    })
-  })
+      }`,
+    }),
+  });
 
-  const json = await res.json()
+  const json = await res.json();
   if (res.status !== 200) {
-    console.error(json)
-    throw new Error('Failed to fetch API')
+    console.error(json);
+    throw new Error('Failed to fetch API');
   }
 
   // [0, 0, 0, 0, 0, 0, 0, 0]
   const reactions = json.data.repository.issue.reactionGroups.map(
     (item) => item.users.totalCount
-  )
+  );
 
   return {
     props: {
-      reactions
+      reactions,
     },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
 
 export default function Home({ reactions }) {
   return (
-    <div className='container'>
+    <div className="container">
       <Head>
         <title>Static Reactions Demo</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta property='og:title' content='Static Reactions Demo' />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:title" content="Static Reactions Demo" />
         <meta
-          property='og:description'
-          content='Using Next.js Incremental Static Regeneration'
+          property="og:description"
+          content="Using Next.js Incremental Static Regeneration"
         />
         <meta
-          property='og:image'
-          content='https://og-image.now.sh/Incremental%20Static%20Regeneration%20Demo%20using%20**GitHub%20Reactions**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
+          property="og:image"
+          content="https://og-image.vercel.app/Incremental%20Static%20Regeneration%20Demo%20using%20**GitHub%20Reactions**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg"
         />
       </Head>
 
@@ -81,55 +81,64 @@ export default function Home({ reactions }) {
         <h2>Static Reactions Demo</h2>
         <h3>
           Reactions on{' '}
-          <a href='https://github.com/chibicode/reactions/issues/1'>
+          <a href="https://github.com/vercel/reactions/issues/1">
             this GitHub issue
           </a>
           :
         </h3>
-        <div className='line'>
-          <span className='emoji'>👍</span> <strong>{reactions[0]}</strong>
+        <div className="line">
+          <span className="emoji">👍</span> <strong>{reactions[0]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>👎</span> <strong>{reactions[1]}</strong>
+        <div className="line">
+          <span className="emoji">👎</span> <strong>{reactions[1]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>😄</span> <strong>{reactions[2]}</strong>
+        <div className="line">
+          <span className="emoji">😄</span> <strong>{reactions[2]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>🎉</span> <strong>{reactions[3]}</strong>
+        <div className="line">
+          <span className="emoji">🎉</span> <strong>{reactions[3]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>😕</span> <strong>{reactions[4]}</strong>
+        <div className="line">
+          <span className="emoji">😕</span> <strong>{reactions[4]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>🧡</span> <strong>{reactions[5]}</strong>
+        <div className="line">
+          <span className="emoji">🧡</span> <strong>{reactions[5]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>🚀</span> <strong>{reactions[6]}</strong>
+        <div className="line">
+          <span className="emoji">🚀</span> <strong>{reactions[6]}</strong>
         </div>
-        <div className='line'>
-          <span className='emoji'>👀</span> <strong>{reactions[7]}</strong>
+        <div className="line">
+          <span className="emoji">👀</span> <strong>{reactions[7]}</strong>
         </div>
         <br />
         <div>
-          <strong>Explanation:</strong> This page is statically generated with <a href="https://nextjs.org/">Next.js</a> by fetching data from GitHub. It’s deployed to{' '}
-          <a href='https://vercel.com/docs/v2/edge-network/overview'>Vercel’s Edge Network</a>{' '}
+          <strong>Explanation:</strong> This page is statically generated with{' '}
+          <a href="https://nextjs.org/">Next.js</a> by fetching data from
+          GitHub. It's deployed to{' '}
+          <a href="https://vercel.com/docs/v2/edge-network/overview">
+            Vercel's Edge Network
+          </a>{' '}
           (CDN). Importantly, this page is being re-generated using{' '}
-          <a href='https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration'>
+          <a href="https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration">
             Incremental Static Regeneration
-          </a> (released in <a href="https://nextjs.org/blog/next-9-5">Next.js 9.5</a>). Here’s how it works:
+          </a>{' '}
+          (released in{' '}
+          <a href="https://nextjs.org/blog/next-9-5">Next.js 9.5</a>). Here's
+          how it works:
         </div>
         <ol>
           <li>
-            Each Next.js page can define the timeout. For this page, it’s set at 1 second.
+            Each Next.js page can define the timeout. For this page, it's set at
+            1 second.
           </li>
           <li>
             When a new request comes in, the statically generated page is
             served.
           </li>
           <li>
-            Later, when another request comes in <strong>after the defined timeout is exceeded</strong>:
-            (1) The statically generated page is served, and (2){' '}
+            Later, when another request comes in{' '}
+            <strong>after the defined timeout is exceeded</strong>: (1) The
+            statically generated page is served, and (2){' '}
             <strong>
               Next.js generates a new version of the page in the background and
               updates the static page for *future* requests
@@ -137,11 +146,23 @@ export default function Home({ reactions }) {
             .
           </li>
           <li>
-            Later, when another request comes in <strong>after the regeneration is done</strong>: The updated static page is served.
+            Later, when another request comes in{' '}
+            <strong>after the regeneration is done</strong>: The updated static
+            page is served.
           </li>
           <li>
             This allows Incremental Static Regeneration on a per-page basis
-            without rebuilding the full app. It’ll always be fast because users will always get a static response. <a href='https://nextjs.org/docs/basic-features/data-fetching#incremental-static-regeneration'>
+            without rebuilding the full app. It'll always be fast because users
+            will always get a static response.{' '}
+            <a href="https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration">
+              Learn more here
+            </a>
+            .
+          </li>
+          <li>
+            <b>Update 2022:</b> Next.js 12.1 now allows you to also revalidate
+            on-demand.{' '}
+            <a href="https://nextjs.org/blog/next-12-1#on-demand-incremental-static-regeneration-beta">
               Learn more here
             </a>
             .
@@ -149,11 +170,13 @@ export default function Home({ reactions }) {
         </ol>
         <div>
           <strong>Source:</strong>{' '}
-          <a href='https://github.com/chibicode/reactions/blob/master/pages/index.js'>
+          <a href="https://github.com/vercel/reactions/blob/master/pages/index.js">
             pages/index.js
-          </a> - `getStaticProps()` fetches the data during static generation, and `revalidate` specifies the timeout.
+          </a>{' '}
+          - `getStaticProps()` fetches the data during static generation, and
+          `revalidate` specifies the timeout.
         </div>
       </main>
     </div>
-  )
+  );
 }
